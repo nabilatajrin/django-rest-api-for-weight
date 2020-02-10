2d46b54a375e647a442d573eb2b2e6defba796e2
nabila <nabilatajrin@gmail.com>
2020-02-10 17:43:12 +0600

    05:43PM 10FEB2020 update

---
+++ django_rest_api/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/user/A/github/django-projects/django-rest-api-for-weight/django_rest_api/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
